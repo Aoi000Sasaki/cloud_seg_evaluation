0e1ad2134c9c5c44a7e3d7007538e384221e4bf6
Aoi000Sasaki <ee203060@meiji.ac.jp>
2024-01-09 01:23:20 +0000

    Refactor cloud_seg_evaluation class and remove unnecessary code

--- include/cloud_seg_evaluation/cloud_seg_evaluation.h
+++ include/cloud_seg_evaluation/cloud_seg_evaluation.h
@@ -2,8 +2,6 @@
 #define CLOUD_SEG_EVALUATION_H
 
 #include <ros/ros.h>
-// #include <sensor_msgs/CameraInfo.h>
-// #include <sensor_msgs/Image.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
@@ -18,39 +16,16 @@ public:
   CloudSegEvaluation();
   ~CloudSegEvaluation();
 
-  void sync_callback(const sensor_msgs::PointCloud2ConstPtr& kitti_cloud_msg,
+  void sync_callback(const sensor_msgs::PointCloud2ConstPtr& correct_cloud_msg,
                      const sensor_msgs::PointCloud2ConstPtr& my_cloud_msg);
-  bool checkLabelConsistency(const sensor_msgs::PointCloud2ConstPtr& cloud1,
-                             const sensor_msgs::PointCloud2ConstPtr& cloud2);
-  // {
-  //   // ポイントクラウドのデータを取得
-  //   pcl::PointCloud<pcl::PointXYZL> pcl_cloud1;
-  //   pcl::fromROSMsg(*cloud1, pcl_cloud1);
-
-  //   pcl::PointCloud<pcl::PointXYZL> pcl_cloud2;
-  //   pcl::fromROSMsg(*cloud2, pcl_cloud2);
-
-  //   // ラベルの一致判定
-  //   for (size_t i = 0; i < pcl_cloud1.size(); ++i)
-  //   {
-  //     if (pcl_cloud1[i].label != pcl_cloud2[i].label)
-  //     {
-  //       // ラベルが一致しない場合
-  //       return false;
-  //     }
-  //   }
-
-  //   // 全ての点のラベルが一致する場合
-  //   return true;
-  // }
+  void checkLabelConsistency(const sensor_msgs::PointCloud2ConstPtr& correct_cloud,
+                             const sensor_msgs::PointCloud2ConstPtr& my_cloud);
 
 private:
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
-  ros::Publisher pub_colored_cloud_;
-  ros::Publisher pub_debug_image_;
   ros::Subscriber sub_camera_info_;
-  message_filters::Subscriber<sensor_msgs::PointCloud2> sub_kitti_cloud_;
+  message_filters::Subscriber<sensor_msgs::PointCloud2> sub_correct_cloud_;
   message_filters::Subscriber<sensor_msgs::PointCloud2> sub_my_cloud_;
   typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2, sensor_msgs::PointCloud2> SyncPolicy;
   message_filters::Synchronizer<SyncPolicy> sync_;
---
+++ src/cloud_seg_evaluation.cpp
@@ -0,0 +1,55 @@
+// Copyright 2023 amsl
+
+#include <ros/ros.h>
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <tf2_ros/transform_listener.h>
+// #include <pcl_ros/transforms.h>
+#include <pcl_ros/point_cloud.h>
+
+#include <cloud_seg_evaluation/cloud_seg_evaluation.h>
+
+namespace cloud_seg_evaluation
+{
+CloudSegEvaluation::CloudSegEvaluation()
+  : pnh_("~")
+  , sub_correct_cloud_(nh_, "correct_cloud", 10)
+  , sub_my_cloud_(nh_, "my_cloud", 10)
+  , sync_(SyncPolicy(10), sub_correct_cloud_, sub_my_cloud_)
+{
+  sync_.registerCallback(boost::bind(&CloudSegEvaluation::sync_callback, this, _1, _2));
+}
+
+CloudSegEvaluation::~CloudSegEvaluation()
+{
+}
+
+void CloudSegEvaluation::sync_callback(const sensor_msgs::PointCloud2ConstPtr& correct_cloud_msg,
+                                          const sensor_msgs::PointCloud2ConstPtr& my_cloud_msg)
+{
+  ROS_INFO("sync_callback");
+}
+
+void CloudSegEvaluation::checkLabelConsistency(const sensor_msgs::PointCloud2ConstPtr& correct_cloud,
+                                                   const sensor_msgs::PointCloud2ConstPtr& my_cloud)
+{
+  // // ポイントクラウドのデータを取得
+  // pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud1;
+  // pcl::fromROSMsg(*cloud1, pcl_cloud1);
+
+  // pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud2;
+  // pcl::fromROSMsg(*cloud2, pcl_cloud2);
+}
+}  // namespace cloud_seg_evaluation
+
+int main(int arg, char** argv)
+{
+  ros::init(arg, argv, "cloud_seg_evaluation");
+  cloud_seg_evaluation::CloudSegEvaluation node;
+  ros::spin();
+  return 0;
+}
